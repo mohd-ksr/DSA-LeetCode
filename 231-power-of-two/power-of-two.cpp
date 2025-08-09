732770d78236fdd3cf4a63829e05d6626548b937
mohd-ksr <153416833+mohd-ksr@users.noreply.github.com>
2025-08-09 19:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.81%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // O(log(n)) solution...
+        // int c=0;
+        // while(n){
+        //     if(n>=INT_MAX || n<0)return false;
+        //     if(n&1==1)c++;
+        //     n=n>>1;
+        // }
+        // return c==1;
+
+        // O(1) solution..
+        return n<1?false:(n&(n-1))==0?true:false;
+    }
+};
