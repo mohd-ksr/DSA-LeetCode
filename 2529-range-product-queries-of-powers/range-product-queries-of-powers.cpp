9bddb3587826482ba7ab71e469305c04a06f6740
mohd-ksr <153416833+mohd-ksr@users.noreply.github.com>
2025-08-11 09:26:05 +0530

    Time: 51 ms (39.36%) | Memory: 147.8 MB (50.53%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    vector<int> createPower(int n){
+        vector<int>powers; int ind=0;
+        while(n){
+            if(n&1)powers.push_back(1<<ind);
+            ind++;
+            n/=2;
+        }
+        return powers;
+    }
+    long long powMod(long long base, long long exp, int mod) {
+        long long res = 1;
+        while (exp > 0) {
+            if (exp & 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return res;
+    }
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        const int MOD=1e9+7;
+        vector<int>powers=createPower(n);
+        int size=powers.size();
+        
+        vector<long long> prefix(size + 1, 1);
+        for (int i = 0; i < size; i++) {
+            prefix[i + 1] = (prefix[i] * powers[i]) % MOD;
+        }
+
+        vector<int>ans;
+        for(auto &q:queries){
+            int l=q[0],r=q[1];
+            long long product=prefix[r+1]*1LL*powMod(prefix[l],MOD-2,MOD)%MOD;
+            ans.push_back(product);
+        }
+        return ans;
+    }
+};
