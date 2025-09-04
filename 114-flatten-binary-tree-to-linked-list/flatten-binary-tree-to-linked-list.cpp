3b18e75f72716e8c2ee466f604164e9aabc5411d
mohd-ksr <153416833+mohd-ksr@users.noreply.github.com>
2025-09-05 00:24:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (95.09%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -10,35 +10,49 @@
  * };
  */
 class Solution {
-private:
-    void dfs(TreeNode*root,stack<TreeNode*>&st){
-        if(!root)return;
-        st.push(root);
-        dfs(root->left,st);
-        dfs(root->right,st);
-    }
-    TreeNode*LL(stack<TreeNode*>&st){
-        TreeNode*head=nullptr;
-        while(!st.empty()){
-            st.top()->left=nullptr;
-            st.top()->right=head;
-            head=st.top();
-            st.pop();
-        }
-        return head;
-    }
+// private:
+//     void dfs(TreeNode*root,stack<TreeNode*>&st){
+//         if(!root)return;
+//         st.push(root);
+//         dfs(root->left,st);
+//         dfs(root->right,st);
+//     }
+//     TreeNode*LL(stack<TreeNode*>&st){
+//         TreeNode*head=nullptr;
+//         while(!st.empty()){
+//             st.top()->left=nullptr;
+//             st.top()->right=head;
+//             head=st.top();
+//             st.pop();
+//         }
+//         return head;
+//     }
 public:
     TreeNode*prev = nullptr;
     void flatten(TreeNode* root) {
         // stack<TreeNode*>st;
         // dfs(root,st);
         // root=LL(st);
-        if(!root)return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
+        // if(!root)return;
+        // flatten(root->right);
+        // flatten(root->left);
+        // root->right = prev;
+        // root->left = nullptr;
+        // prev = root;
+
+        // by morris traversel
+        TreeNode*curr = root;
+        while(curr){
+            if(curr->left){
+                prev = curr->left;
+                while(prev->right) prev = prev->right;
+                prev->right = curr->right;
+                curr->right = curr->left; 
+                curr->left = nullptr;
+            }
+            curr = curr->right;
+        }
     }
 };
-// Time and Space Complexity O(n)
+// Time Complexity O(n) 
+// Space Complexity O(n) and for morris traversal O(1)
