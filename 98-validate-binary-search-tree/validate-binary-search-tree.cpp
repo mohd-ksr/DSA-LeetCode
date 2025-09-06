864fd06e72e9a7bc35b5d54e0430dd6f52499fa7
mohd-ksr <153416833+mohd-ksr@users.noreply.github.com>
2025-09-06 21:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (17.73%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool check(TreeNode* root, long long mini, long long maxi){
+        if(!root) return true;
+        if(root->val <= mini || root->val >= maxi) return false;
+        return check(root->left,mini,root->val) && check(root->right,root->val,maxi);
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};
+// Time complexity O(n)
+// space complexity O(n) for recursive stack
