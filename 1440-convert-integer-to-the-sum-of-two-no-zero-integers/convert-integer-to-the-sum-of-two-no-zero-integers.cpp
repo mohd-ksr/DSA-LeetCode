7917f2aa052b99394516c1453192d1b8ed3861d4
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-09-08 13:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.27%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    bool notContains0(int n){
+        while(n){
+            if(n%10==0)return false;
+            n/=10;
+        }
+        return true;
+    }
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for(int i=1; i<=n; i++){
+            int f = i, s = n-i;
+            if(notContains0(f) && notContains0(s)) return {f,s};
+        }
+        return {}; //dummy  
+    }
+};
+// Time complexity O(n)
