57863c2ca5e38910c14d61bdba103b9062bbb9a9
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-09-12 14:01:59 +0530

    Time: 9 ms (43.66%) | Memory: 19.2 MB (25.75%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,16 +1,11 @@
 class Solution {
-private:
-    int countVowels(string &s){
-        int count = 0;
-        for(auto &ch:s){
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')count++;
-        }
-        return count;
-    }
 public:
     bool doesAliceWin(string s) {
-        int vCnt = countVowels(s);
-        return vCnt != 0;
-
+        for(auto &ch:s){
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')return true;
+        }
+        return false;
     }
 };
+// Time Comlexity O(n)
+// Space Complexity O(1)
