46515ae537453e5049dee5c82cc4674e42f73baf
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-09-22 11:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (9.87%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for(auto n: nums){
+        // unordered_map<int, int> freq;
+        // for(auto n: nums){
+        //     freq[n]++;
+        // }
+        // int maxi = 0;
+        // for(auto it: freq){
+        //     maxi = max(maxi, it.second);
+        // }
+
+        // int count = 0;
+        // for(auto it: freq){
+        //     if(it.second==maxi){
+        //         count++;
+        //     }
+        // }
+
+        // return count*maxi;
+        vector<int>freq(101);
+        int maxi=0, cnt=0,ans=0;
+        for(auto &n:nums){
             freq[n]++;
+            maxi=max(maxi,freq[n]);
         }
-        int maxi = 0;
-        for(auto it: freq){
-            maxi = max(maxi, it.second);
-        }
-
-        int count = 0;
-        for(auto it: freq){
-            if(it.second==maxi){
-                count++;
-            }
+        for(auto &f:freq){
+            if(maxi==f)cnt++;
+            ans=maxi*cnt;
         }
 
-        return count*maxi;
-
-        
+        return ans;
     }
 };
