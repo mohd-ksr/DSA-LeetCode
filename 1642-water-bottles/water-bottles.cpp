adc52ec7da137008887dc33c5622c1a42ef791d2
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-10-01 23:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = 0;
+        int empty = 0;
+        int full = numBottles;
+        while(full>0){
+            ans+=full;
+            empty+=full;
+            full=0;
+            full = empty / numExchange;
+            empty = empty % numExchange;
+        }
+        return ans;
+    }
+};
