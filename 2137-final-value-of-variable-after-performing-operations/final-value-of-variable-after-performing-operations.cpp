1d476fcd2791ea115460bc2912f9cbbb1d5711c9
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-10-20 09:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (42.92%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans=0;
+        for(auto &it:operations){
+            ans+=(it[1]=='+'?1:-1);
+        }
+        return ans;
+    }
+};
+// Time Complexity O(n)
