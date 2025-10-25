3a2332f9a5e83c1816d0a9b53dc9dcffd24b371f
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-10-25 09:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.24%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int completeWeek = n/7;
+        int remainingDays = n%7;
+        int countWeek = 1;
+        long long ans = 0;
+        while(countWeek <= completeWeek){
+            ans+=(countWeek*7 + 21);
+            countWeek++;
+        }
+        ans+=(countWeek*remainingDays + remainingDays*(remainingDays-1)/2);
+        return ans;
+    }
+};
