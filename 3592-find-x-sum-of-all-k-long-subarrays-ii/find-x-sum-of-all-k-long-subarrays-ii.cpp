94cf003375d268a7ee33f555da08ce403596b023
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-11-05 23:10:54 +0530

    Time: 901 ms (34.61%) | Memory: 349.2 MB (11.54%) - LeetSync

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -10,10 +10,10 @@ public:
     vector<long long> findXSum(vector<int>& nums, int k, int x) {
         int n = nums.size();
         vector<long long> ans;
-        unordered_map<long long,long long> freq; // value -> count
+        unordered_map<long long,long long> freq;
 
-        set<pair<long long,long long>, Comp> top;   // top x by (freq, value)
-        set<pair<long long,long long>, Comp> rest;  // the others
+        set<pair<long long,long long>, Comp> top;   
+        set<pair<long long,long long>, Comp> rest; 
         long long topSum = 0;
 
         auto removeOldPair = [&](long long val, long long oldf) {
@@ -21,7 +21,7 @@ public:
             pair<long long,long long> p = {oldf, val};
             auto it = top.find(p);
             if (it != top.end()) {
-                topSum -= it->first * it->second; // freq * val
+                topSum -= it->first * it->second; 
                 top.erase(it);
             } else {
                 auto it2 = rest.find(p);
@@ -35,24 +35,18 @@ public:
         };
 
         auto rebalance = [&]() {
-            // Fill top up to x from rest
             while ((int)top.size() < x && !rest.empty()) {
                 auto best = *rest.begin();
                 rest.erase(rest.begin());
                 top.insert(best);
                 topSum += best.first * best.second;
             }
-            // Ensure every element in top is better than every element in rest
             while (!rest.empty() && !top.empty()) {
                 auto bestRest = *rest.begin();
                 auto worstTopIt = prev(top.end());
                 auto worstTop = *worstTopIt;
-                // if bestRest is better (cmp says comes earlier) than worstTop, swap them
-                // comparator orders higher first; so compare using Comp()
                 Comp cmp;
-                // bestRest better than worstTop if cmp(bestRest, worstTop) == true
                 if (cmp(bestRest, worstTop)) {
-                    // swap
                     rest.erase(rest.begin());
                     top.erase(worstTopIt);
                     top.insert(bestRest);
@@ -61,7 +55,6 @@ public:
                     topSum -= worstTop.first * worstTop.second;
                 } else break;
             }
-            // If top has more than x (possible if we removed others incorrectly), move worst to rest
             while ((int)top.size() > x) {
                 auto worstTopIt = prev(top.end());
                 auto worstTop = *worstTopIt;
@@ -74,9 +67,7 @@ public:
         for (int i = 0; i < n; ++i) {
             long long v = nums[i];
             long long oldf = freq[v];
-            // remove old pair (from top or rest)
             removeOldPair(v, oldf);
-            // update freq and insert new as rest (we always insert into rest and then rebalance)
             freq[v] = oldf + 1;
             insertNewPairAsRest(v, freq[v]);
 
