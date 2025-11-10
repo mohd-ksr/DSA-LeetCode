30183d6e6d813bf1102a2f999a6ba60534ca1373
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-11-10 11:07:01 +0530

    Time: 27 ms (80.62%) | Memory: 222 MB (87.36%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        vector<int>s;
+        for(int n:nums){
+            while(!s.empty() && s.back()>n)s.pop_back();
+            if(n==0)continue;
+            if(s.empty() || s.back() < n){
+                ans++;
+                s.push_back(n);
+            }
+        }
+        return ans;
+    }
+};
