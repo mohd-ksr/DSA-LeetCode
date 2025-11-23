aaf9f21fd88f3b95b4f498e1e8f72f3f875672a5
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-11-23 20:42:44 +0530

    Time: 9 ms (68.68%) | Memory: 31.3 MB (62.96%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        vector<int> modGroup[3];
+
+        for (int num : nums) 
+            modGroup[num % 3].push_back(num);
+
+        sort(modGroup[1].begin(), modGroup[1].end(), greater<int>());
+        sort(modGroup[2].begin(), modGroup[2].end(), greater<int>());
+
+        int maxSum = 0;
+        int size1 = modGroup[1].size(), size2 = modGroup[2].size();
+
+        for (int count1 = size1 - 2; count1 <= size1; ++count1) {
+            if (count1 >= 0) {
+                for (int count2 = size2 - 2; count2 <= size2; ++count2) {
+                    if (count2 >= 0 && (count1 - count2) % 3 == 0) {
+                        maxSum = max(
+                            maxSum,
+                            accumulate(modGroup[1].begin(), modGroup[1].begin() + count1, 0) +
+                            accumulate(modGroup[2].begin(), modGroup[2].begin() + count2, 0)
+                        );
+                    }
+                }
+            }
+        }
+
+        return maxSum + accumulate(modGroup[0].begin(), modGroup[0].end(), 0);
+    }
+};
