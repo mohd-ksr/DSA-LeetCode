578ffa31e3de1529c398933ebf35e66be9a3976b
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-12-02 19:38:05 +0530

    Time: 47 ms (66.19%) | Memory: 52.1 MB (89.24%) - LeetSync

---
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    bool p(int n, vector<int>& price, int k){
+        k--;
+        int temp = price[0];
+        for(int i=1; i<price.size(); i++){
+            if(price[i]-temp>=n){
+                temp=price[i];
+                if(--k==0)return true;
+            }
+        }
+        return false;
+    }
+public:
+    int maximumTastiness(vector<int>& price, int k) {
+        sort(price.begin(), price.end());
+        int s=0, e=price[price.size()-1]-price[0];
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(p(mid,price,k))s=mid+1;
+            else e=mid-1;
+        }
+        return e;
+    }
+};
