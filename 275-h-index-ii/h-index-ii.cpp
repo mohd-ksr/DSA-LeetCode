7c2202e903f779ce6bb8f2a6be0f8ad58c8573d3
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2025-12-02 20:17:16 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (46.22%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int s=0, e=n-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(citations[mid]<(n-mid))s=mid+1;
+            else e=mid-1;
+        }
+        return n-s;
+    }
+};
+// Time 
