d2550c565b11da19df7621ab64c6f62027c80990
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-19 22:32:27 +0530

    Time: 39 ms (20.51%) | Memory: 89 MB (18.50%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,17 +1,18 @@
 class Solution {
 private: 
-    int solve(int n, int curLen, int pasteLen){
+    int solve(int n, int curLen, int pasteLen, vector<vector<int>>&dp){
         if(curLen==n)return 0;
         if(curLen>n)return 1000;
+        if(dp[curLen][pasteLen]!=0)return dp[curLen][pasteLen];
+        int opt1 = 2+solve(n, curLen*2, curLen,dp);
+        int opt2 = 1 + solve(n, curLen+pasteLen, pasteLen,dp);
 
-        int opt1 = 2+solve(n, curLen*2, curLen);
-        int opt2 = 1 + solve(n, curLen+pasteLen, pasteLen);
-
-        return min(opt1, opt2);
+        return dp[curLen][pasteLen]=min(opt1, opt2);
     }
 public:
     int minSteps(int n) {
         if(n==1)return 0;
-        return 1+solve(n,1,1);
+        vector<vector<int>>dp(n+1, vector<int>(n/2+1, 0));
+        return 1+solve(n,1,1,dp);
     }
 };
