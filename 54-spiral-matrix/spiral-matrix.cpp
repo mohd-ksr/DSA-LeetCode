2d376b74759d6b38ec8fcf65a4252f0674544a66
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-19 21:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (80.07%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        int sr=0;
-        int sc=0;
-        int er=row-1;
-        int ec=col-1;
-        int count=0;
-        int maxCount=row*col;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int sr = 0;
+        int sc = 0;
+        int er = row-1;
+        int ec = col-1;
+        int count = 0;
+        int maxCount = row*col;
         vector<int>ans;
         while(count<maxCount){
             for(int i=sc; i<=ec && count<maxCount; i++){
@@ -35,5 +35,44 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+//         int row=matrix.size();
+//         int col=matrix[0].size();
+//         int sr=0;
+//         int sc=0;
+//         int er=row-1;
+//         int ec=col-1;
+//         int count=0;
+//         int maxCount=row*col;
+//         vector<int>ans;
+//         while(count<maxCount){
+//             for(int i=sc; i<=ec && count<maxCount; i++){
+//                 ans.push_back(matrix[sr][i]);
+//                 count++;
+//             }
+//             sr++;
+//             for(int i=sr; i<=er && count<maxCount; i++){
+//                 ans.push_back(matrix[i][ec]);
+//                 count++;
+//             }
+//             ec--;
+//             for(int i=ec; i>=sc && count<maxCount; i--){
+//                 ans.push_back(matrix[er][i]);
+//                 count++;
+//             }
+//             er--;
+//             for(int i=er; i>=sr && count<maxCount; i--){
+//                 ans.push_back(matrix[i][sc]);
+//                 count++;
+//             }
+//             sc++;
+//         }
+//         return ans;
+//     }
+// };
 // Time Complexity O(mxn)
 // Time Complexity O(mxn) for answer vector
