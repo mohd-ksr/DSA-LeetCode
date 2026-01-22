94a240172dfe0e82b7503883b9816c3a8ce57caa
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-22 22:48:38 +0530

    Time: 243 ms (19.32%) | Memory: 12.6 MB (90.08%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>isPrime(n+1, true);
+        int ans = 0;
+        for(int i=2; i<n; i++){
+            if(isPrime[i]){
+                ans++;
+                for(int j = i*2; j<=n; j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        return ans;
+    }
+};
