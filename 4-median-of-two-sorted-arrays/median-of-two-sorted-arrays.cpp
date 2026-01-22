ca8510e570edd6a451fc4621118c2b44e518fec2
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-22 21:48:16 +0530

    Time: 0 ms (100.00%) | Memory: 94.9 MB (93.55%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>temp(nums1.size()+nums2.size());
-        int i=0,j=0,k=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                temp[k++]=nums1[i++];
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1>n2)return findMedianSortedArrays(nums2, nums1);
+
+        int s = 0, e = n1;
+        int left = (n1+n2+1)/2;
+        int n = n1 + n2;
+        while(s<=e){
+            int mid1 = s+(e-s)/2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1-1 >=0) l1 = nums1[mid1-1];
+            if(mid2-1 >=0) l2 = nums2[mid2-1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n%2 == 1)return max(l1,l2);
+                else return (double)(max(l1, l2) + min(r1, r2))/2.0;
             }
-            else{
-                temp[k++]=nums2[j++];
-            }
+            else if(l1>r2)e = mid1 - 1;
+            else s = mid1 + 1;
         }
-        while(i<nums1.size()){
-            temp[k++]=nums1[i++];
-        }
-        while(j<nums2.size()){
-            temp[k++]=nums2[j++];
-        }
-        int n=temp.size();
-        if(n%2==0){
-            return (temp[n/2-1]+temp[n/2])/2.0;
-        }
-        else{
-            return 1.0*temp[n/2];
-        }
+        return 0.0;
     }
 };
-// Time Complexity(max(n,m))
-// Sapce Complexity(m+n)
