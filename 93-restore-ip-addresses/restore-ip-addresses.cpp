30d5f1832de60ff56c938e575db452c69965ebef
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-23 15:49:13 +0530

    Time: 1 ms (48.29%) | Memory: 10.8 MB (46.14%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:   
+    vector<string>ans;
+    vector<string>parts;
+    void bt(string &s, int ind){
+        if(parts.size()==4){
+            if(ind==s.size()){
+                ans.push_back(parts[0]+"."+parts[1]+"."+parts[2]+"."+parts[3]);
+            }
+             return;
+        }
+
+        int rp = 4 - parts.size();
+        int rc = s.size()-ind;
+
+        if(rc<rp || rc>rp*3) return ;
+
+        for(int len = 1; len<=3 && ind+len<=s.size(); len++){
+            string part = s.substr(ind, len);
+
+            if(part.size()>1 && part[0]=='0')break;
+
+            int temp = stoi(part);
+            if(temp>255)break;
+
+            parts.push_back(part);
+            bt(s, ind+len);
+            parts.pop_back();
+        }
+    }
+public:
+    vector<string> restoreIpAddresses(string s) {
+        bt(s, 0);
+        return ans;
+    }
+};
