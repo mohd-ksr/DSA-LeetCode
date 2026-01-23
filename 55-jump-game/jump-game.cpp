91f126428bf1d3085a85d739dda65c953f8c7cdf
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-23 11:33:17 +0530

    Time: 615 ms (7.41%) | Memory: 55.8 MB (6.23%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    bool solve(int ind, vector<int>&nums, vector<int>&dp){
+        if(ind==nums.size()-1)return true;
+        if(ind>=nums.size())return false;
+        if(dp[ind]!=-1)return dp[ind];
+        bool ans = false;
+        for(int i=1; i<=nums[ind]; i++){
+           if(solve(ind+i, nums, dp))return dp[ind]=true;
+        }
+        return dp[ind]=false;
+
+    }
+public:
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size()+1, -1);
+        return solve(0, nums, dp);
+    }
+};
