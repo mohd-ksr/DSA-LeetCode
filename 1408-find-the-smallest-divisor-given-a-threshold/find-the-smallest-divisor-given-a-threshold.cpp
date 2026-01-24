12a5e038e38c392b1c829261279c87e260ab1b62
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-24 17:58:35 +0530

    Time: 9 ms (49.21%) | Memory: 26.1 MB (78.79%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,20 +1,22 @@
 class Solution {
-private:
-    bool isPossible(vector<int>& nums, int d, int th){
-        int res = 0;
-        for(int n:nums){
-            res+=ceil((double)n/(double)d);
+private:   
+    int isPossible(vector<int>& nums, int div, int threshold){
+        int sum = 0;
+        for(auto &n:nums){
+            sum += ceil((1.0*n)/div);
         }
-        return res<=th;
+        return sum<=threshold;
     }
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
         int s = 1, e = *max_element(nums.begin(), nums.end());
-        while(s<=e){
+        while(s<e){
             int mid = s+(e-s)/2;
-            if(isPossible(nums,mid,threshold))e=mid-1;
-            else s = mid+1;
+            if(isPossible(nums, mid, threshold)){
+                e=mid;
+            }
+            else s=mid+1;
         }
-        return s;
+        return e;
     }
 };
