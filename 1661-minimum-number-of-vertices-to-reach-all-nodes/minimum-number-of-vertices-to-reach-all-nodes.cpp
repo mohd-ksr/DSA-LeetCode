1b139885662a080ed8e31a6d50026cfa06ae937a
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-26 22:59:43 +0530

    Time: 4 ms (66.69%) | Memory: 98.9 MB (79.37%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<bool>vis(n, true);
-        for(auto e:edges){
-            vis[e[1]]=false;
+        vector<int>cand(n, true);
+        for(auto &e:edges){
+            cand[e[1]]=false;
         }
+
         vector<int>ans;
-       for(int i=0; i<n; i++){
-        if(vis[i])ans.push_back(i);
-       }
-       return ans;
+        for(int i=0; i<n; i++){
+            if(cand[i])ans.push_back(i);
+        }
+
+        return ans;
     }
 };
