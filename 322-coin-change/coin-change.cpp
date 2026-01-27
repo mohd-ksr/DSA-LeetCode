c558d707f43a870372d5c3b4c07e8aed8ff96337
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-27 13:22:31 +0530

    Time: 47 ms (33.10%) | Memory: 52.9 MB (14.87%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,41 @@
 class Solution {
 private:
-    int dfs(vector<int>& coins, int n, int amt, vector<vector<int>>&dp){
-        if(amt==0)return 0;
-        if(n<0)return 1e9;
-        if(dp[n][amt]!=-1)return dp[n][amt];
-        int notPick = dfs(coins, n-1, amt, dp);
-        int pick = 1e9;
-        if(amt>=coins[n]){
-            int temp = dfs(coins, n, amt-coins[n],dp);
-            if(temp!=1e9)pick=1+temp;
+    const int inf = 1e9;
+    int solve(int ind, int sum, vector<int>& coins, vector<vector<int>>&dp){
+        if(sum==0)return 0;
+        if(ind<=0)return inf;
+        if(dp[ind-1][sum]!=-1)return dp[ind-1][sum];
+        if(coins[ind-1]<=sum){
+            return dp[ind-1][sum]=min(1+solve(ind, sum-coins[ind-1], coins, dp), solve(ind-1, sum, coins, dp));
         }
-        return dp[n][amt]=min(pick, notPick);
+        else return dp[ind-1][sum]=solve(ind-1, sum, coins, dp);
+
     }
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size()+1, vector<int>(amount+1, -1));
-        int ans = dfs(coins, coins.size()-1, amount, dp);
-        if(ans>=1e9)return -1;
-        return ans;
+    int coinChange(vector<int>& coins, int sum) {
+        // int n = coins.size();
+        // vector<vector<int>>dp(n+1, vector<int>(sum+1, -1));
+        // int res = solve(n, sum, coins, dp);
+        // return (res==inf? -1:res);
+
+        int n = coins.size();
+        vector<vector<int>>dp(n+1, vector<int>(sum+1, inf));
+        for(int i=0; i<=n; i++){
+            dp[i][0]=0;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return (dp[n][sum]==inf? -1:dp[n][sum]);
+        
     }
 };
+
+
+
