4824ae65adec337e43e91d331e005ef6bb659eab
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-28 10:49:21 +0530

    Time: 20 ms (46.66%) | Memory: 28.3 MB (73.42%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans = "";
+        for(auto &ch:s){
+            if(ch=='*')ans.pop_back();
+            else ans.push_back(ch);
+        }
+        return ans;
+    }
+};
