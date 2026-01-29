a90c0fcd927327fe77c82dc9918030f9272b0ca4
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-29 14:20:07 +0530

    Time: 11 ms (65.49%) | Memory: 16.8 MB (55.70%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,18 +1,21 @@
 class Solution {
-public:
-    int minDistance(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        
-        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-            }
+private:    
+    int lcs(int n, int m, string &s1, string &s2, vector<vector<int>>&dp){
+        if(n==0 || m==0)return 0;
+        if(dp[n][m]!=-1)return dp[n][m];
+
+        if(s1[n-1]==s2[m-1]){
+            return dp[n][m] = 1 + lcs(n-1, m-1, s1, s2, dp);
         }
-        
-        return n+m-2*dp[n][m];
+        else{
+            return dp[n][m] = max(lcs(n-1, m, s1, s2, dp), lcs(n, m-1, s1, s2, dp));
+        }
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        int res = lcs(n, m, word1, word2, dp);
+        return m+n-2*res;
     }
 };
