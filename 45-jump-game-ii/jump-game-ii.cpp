6c2a35742d87e7b2a0c2a37a2c826ab91e7142d3
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-30 11:50:49 +0530

    Time: 270 ms (5.00%) | Memory: 22.4 MB (9.69%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
-private:   
-    const int inf = 1e4+1;
-    int solve(int ind, vector<int>& nums,vector<int>&dp){
+private:
+    const int inf = 1e9+7;
+    int solve(int ind, vector<int>&nums,  vector<int>&dp){
         if(ind>=nums.size()-1)return 0;
         if(nums[ind]==0)return inf;
-        
         if(dp[ind]!=-1)return dp[ind];
-
         int ans = inf;
         for(int i=1; i<=nums[ind]; i++){
-            ans = min(ans, 1+solve(ind+i, nums,dp));
+            ans = min(ans, 1+solve(ind+i, nums, dp));
         }
         return dp[ind]=ans;
     }
 public:
     int jump(vector<int>& nums) {
         vector<int>dp(nums.size()+1, -1);
-        return solve(0,nums,dp);
+        return solve(0, nums, dp);
     }
 };
