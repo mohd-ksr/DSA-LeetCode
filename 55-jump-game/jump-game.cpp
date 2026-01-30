6cc142dcdb3b41f7494334a7a05f284cc2a2489c
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-30 11:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (21.27%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,28 +1,11 @@
 class Solution {
-private:
-    bool solve(int ind, vector<int>&nums, vector<int>&dp){
-        if(ind==nums.size()-1)return true;
-        if(ind>=nums.size())return false;
-        if(dp[ind]!=-1)return dp[ind];
-        bool ans = false;
-        for(int i=1; i<=nums[ind]; i++){
-           if(solve(ind+i, nums, dp))return dp[ind]=true;
-        }
-        return dp[ind]=false;
-
-    }
 public:
     bool canJump(vector<int>& nums) {
-        // vector<int>dp(nums.size()+1, -1);
-        // return solve(0, nums, dp);
-        int khaTakJaSaktaHai = 0;
-        for(int abhiYhaHai = 0; abhiYhaHai<nums.size(); abhiYhaHai++){
-            if(abhiYhaHai > khaTakJaSaktaHai)return false;
-            khaTakJaSaktaHai = max(khaTakJaSaktaHai, nums[abhiYhaHai]+abhiYhaHai);
+        int ind = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>ind)return false;
+            ind = max(ind, nums[i]+i);
         }
         return true;
-        
     }
 };
-
-// Time Complexity O(n)
