f2e9f5d3ae7fc6d845d3774ee423611395d99e44
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-01-31 11:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (9.52%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -10,49 +10,28 @@
  * };
  */
 class Solution {
-// private:
-//     void dfs(TreeNode*root,stack<TreeNode*>&st){
-//         if(!root)return;
-//         st.push(root);
-//         dfs(root->left,st);
-//         dfs(root->right,st);
-//     }
-//     TreeNode*LL(stack<TreeNode*>&st){
-//         TreeNode*head=nullptr;
-//         while(!st.empty()){
-//             st.top()->left=nullptr;
-//             st.top()->right=head;
-//             head=st.top();
-//             st.pop();
-//         }
-//         return head;
-//     }
-public:
-    TreeNode*prev = nullptr;
-    void flatten(TreeNode* root) {
-        // stack<TreeNode*>st;
-        // dfs(root,st);
-        // root=LL(st);
-        // if(!root)return;
-        // flatten(root->right);
-        // flatten(root->left);
-        // root->right = prev;
-        // root->left = nullptr;
-        // prev = root;
-
-        // by morris traversel
-        TreeNode*curr = root;
-        while(curr){
-            if(curr->left){
-                prev = curr->left;
-                while(prev->right) prev = prev->right;
-                prev->right = curr->right;
-                curr->right = curr->left; 
-                curr->left = nullptr;
-            }
-            curr = curr->right;
+private:
+    void preOrder(TreeNode*root, stack<TreeNode*>&st){
+        while(!root)return;
+        st.push(root);
+        preOrder(root->left, st);
+        preOrder(root->right, st);
+    }
+    TreeNode* createLL(stack<TreeNode*>&st){
+        TreeNode * head=nullptr;
+        while(!st.empty()){
+            TreeNode* node = st.top(); st.pop();
+            node->left = nullptr;
+            node->right = head;
+            head=node;
         }
+        return head;
+
+    }
+public:
+    void flatten(TreeNode* root) {
+        stack<TreeNode*>st;
+        preOrder(root, st);
+        root= createLL(st);
     }
 };
-// Time Complexity O(n) 
-// Space Complexity O(n) and for morris traversal O(1)
