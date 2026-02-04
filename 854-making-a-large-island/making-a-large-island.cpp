4b2519a8988c23518bb0926df6d7f269c6790dfb
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-04 12:25:48 +0530

    Time: 241 ms (58.84%) | Memory: 149.9 MB (55.99%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,38 +1,37 @@
 class Solution {
 private:
     int r,c;
-    int dfs(int i, int j, vector<vector<int>>&grid, int &key){
+    int dfs(vector<vector<int>>&grid, int i, int j, int &key){
         if(i<0 || i>=r || j<0 || j>=c || grid[i][j]!=1)return 0;
         grid[i][j]=key;
-        return 1+dfs(i-1, j, grid, key)+dfs(i, j-1, grid, key)+dfs(i+1, j, grid, key)+dfs(i, j+1, grid, key);
+        return 1 + dfs(grid, i-1, j, key) + dfs(grid, i, j-1, key) + dfs(grid, i+1, j, key) + dfs(grid, i, j+1, key);
     }
 public:
     int largestIsland(vector<vector<int>>& grid) {
         this->r = grid.size();
         this->c = grid[0].size();
-
         unordered_map<int, int>mapp;
         int key=2;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 if(grid[i][j]==1){
-                    mapp[key]=dfs(i, j, grid, key);
+                    mapp[key]=dfs(grid, i, j, key);
                     key++;
                 }
             }
         }
-        if(mapp.size()==0)return 1;
 
+        if(mapp.size()==0)return 1;
         if(mapp.size()==1){
-            return( mapp[2]==r*c?mapp[2]:mapp[2]+1);
+            return (mapp[2]==r*c? mapp[2]:mapp[2]+1);
         }
 
         int ans = 1;
+
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 if(grid[i][j]==0){
                     unordered_set<int>st;
-                    int tempAns = 1;
                     if(i-1>=0 && grid[i-1][j]>1){
                         st.insert(grid[i-1][j]);
                     }
@@ -46,10 +45,11 @@ public:
                         st.insert(grid[i][j+1]);
                     }
 
-                    for(auto id:st){
-                        tempAns+=mapp[id];
+                    int temp = 1;
+                    for(auto it:st){
+                        temp+=mapp[it];
                     }
-                    ans = max(ans, tempAns);
+                    ans = max(ans, temp);
                 }
             }
         }
