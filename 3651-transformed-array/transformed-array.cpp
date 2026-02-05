a7da50c43622a62c8e0a82dd2b6e61819c2afda6
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-05 09:35:08 +0530

    Time: 4 ms (79.67%) | Memory: 25.8 MB (8.08%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(n);
+        for(int i=0; i<n; i++){
+            int e = nums[i];
+            int ind = ((i+nums[i])%n + n)%n;
+            ans[i]=nums[ind];
+        }
+
+        return ans;
+
+    }
+};
