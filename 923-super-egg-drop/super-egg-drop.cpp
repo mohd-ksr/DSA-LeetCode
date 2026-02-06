3f1e29b2aa519a1a0ee6a035d0dfca2ce3419f99
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-06 15:01:31 +0530

    Time: 47 ms (55.13%) | Memory: 32.4 MB (35.48%) - LeetSync

---
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -0,0 +1,52 @@
+class Solution {
+private:
+    int solve(int eggs, int floors, vector<vector<int>>&dp){
+        if(floors<=1 || eggs==1)return floors;
+        if(dp[eggs][floors]!=-1)return dp[eggs][floors];
+
+        // int ans = INT_MAX;
+        // for(int i=1; i<=floors; i++){
+        //     int b,nb;
+        //     if(dp[eggs-1][i-1]!=-1){
+        //         b=dp[eggs-1][i-1];
+        //     }
+        //     else{
+        //         b=solve(eggs-1, i-1, dp);
+        //         dp[eggs-1][i-1]=b;
+        //     }
+
+        //     if(dp[eggs][floors-i]!=-1){
+        //         nb=dp[eggs][floors-i];
+        //     }
+        //     else{
+        //         nb=solve(eggs, floors-i, dp);
+        //         dp[eggs][floors-i]=nb;
+        //     }
+
+        //     ans = min(ans, 1+max(b, nb));
+        // }
+
+        int s=0, e=floors, ans=INT_MAX;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+
+            int b = solve(eggs-1, mid-1, dp);
+            int nb = solve(eggs, floors-mid, dp);
+
+            if(b<nb){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+
+            ans = min(ans, 1+max(b, nb));
+        }
+        return dp[eggs][floors]=ans;
+    }
+public:
+    int superEggDrop(int eggs, int floors) {
+        vector<vector<int>>dp(eggs+1, vector<int>(floors+1, -1));
+        return solve(eggs, floors, dp);
+    }
+};
