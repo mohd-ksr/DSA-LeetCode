bb1969c140902db5ba91b99f440477c0daa1d910
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-07 11:39:58 +0530

    Time: 437 ms (5.04%) | Memory: 172.5 MB (18.47%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,23 +1,27 @@
 class Solution {
-private: 
-    int MOD = 1e9 + 7;
-    int dfs(vector<vector<int>>& grid, int &n, int &m, int i, int j, int rem, int &k, vector<vector<vector<int>>>&dp){
-        if(i<0 || i>=n || j<0 || j>=m)return 0;
-        if(i==n-1 && j==m-1){
-            return (rem+grid[i][j])%k==0;
+private:
+    int r, c;
+    const int mod = 1e9+7;
+    int dfs(vector<vector<int>>& grid, int i, int j, int sum, int &k, vector<vector<bool>>&vis, vector<vector<vector<int>>>&dp){
+        if(i<0 || i>=r || j<0 || j>=c || vis[i][j])return 0;
+        sum+=grid[i][j];
+        if(i==r-1 && j==c-1){
+            return sum%k==0;
         }
-        if(dp[i][j][rem]!=-1)return dp[i][j][rem];
-        long long ans=0;
-        ans+=dfs(grid,n,m,i+1,j,(rem+grid[i][j])%k,k,dp);
-        ans+=dfs(grid,n,m,i,j+1,(rem+grid[i][j])%k,k,dp);
-        return dp[i][j][rem] = ans%MOD;
+        if(dp[i][j][sum%k]!=-1)return dp[i][j][sum%k];
+        vis[i][j]=true;
+        int right = dfs(grid, i, j+1, sum, k, vis, dp);
+        int down = dfs(grid, i+1, j, sum, k, vis, dp);
+        vis[i][j]=false;
+        return  dp[i][j][sum%k]=(right%mod + down%mod)%mod;
     }
 public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
-        return dfs(grid,n,m,0,0,0,k,dp);
+        this->r = grid.size();
+        this->c = grid[0].size();
+        vector<vector<bool>>vis(r, vector<bool>(c, false));
+        // vector<vector<int>>dp(r, vector<int>(c, -1));
+        vector<vector<vector<int>>>dp(r+1, vector<vector<int>>(c+1, vector<int>(k+1, -1)));
+        return dfs(grid, 0, 0, 0, k, vis, dp);
     }
 };
-// Time Complexity O(n*m*k)
-// Space Complexity O(n*m*k)
