56f3f24d4521287854c9875db1261728d78b7a32
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-07 10:41:24 +0530

    Time: 15 ms (82.84%) | Memory: 25.4 MB (87.57%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        vector<int>A(n,0), B(n, 0);
-        int a=0, b=0;
-        for(int i=0; i<n; i++){
-            B[i]=b;
-            A[n-i-1]=a;
-            if(s[i]=='b')b++;
-            if(s[n-i-1]=='a')a++;
+        // int n = s.length();
+        // vector<int>A(n,0), B(n, 0);
+        // int a=0, b=0;
+        // for(int i=0; i<n; i++){
+        //     B[i]=b;
+        //     A[n-i-1]=a;
+        //     if(s[i]=='b')b++;
+        //     if(s[n-i-1]=='a')a++;
 
+        // }
+
+        // int ans = n;
+        // for(int i=0; i<n; i++){
+        //     ans = min(ans, A[i]+B[i]);
+        // }
+        int ans = 0;
+        int b=0;
+        for(auto &ch:s){
+            if(ch=='b')b++;
+            else if(b!=0){
+                b--;
+                ans++;
+            }
         }
-
-        int ans = n;
-        for(int i=0; i<n; i++){
-            ans = min(ans, A[i]+B[i]);
-        }
-
         return ans;
     }
 };
