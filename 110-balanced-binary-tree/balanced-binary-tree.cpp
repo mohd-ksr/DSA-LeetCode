9a7700aecc0ca6bdb157e15a5b3bb96f0634c585
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-08 21:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (82.16%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,28 +11,18 @@
  */
 class Solution {
 private:
-    int solve(TreeNode*root){
+    int solve(TreeNode* root){
         if(!root)return 0;
-        int l=solve(root->left);
+        int l = solve(root->left);
         if(l==-1)return -1;
-        int r=solve(root->right);
+        int r = solve(root->right);
         if(r==-1)return -1;
         if(abs(l-r)>1)return -1;
-        return 1+max(l,r);
+
+        return max(l, r)+1;
     }
 public:
     bool isBalanced(TreeNode* root) {
-        if(!root)return true;
-        // BruteForce-- Time complexity O(n^2)
-        // int l=height(root->left);
-        // int r=height(root->right);
-        // if(abs(l-r)>1)return false;
-        // bool le=isBalanced(root->left);
-        // bool ri=isBalanced(root->right);
-        // if(!le || !ri)return false;
-        // return true;
-        
-        // Logic of finding hieght of BT-- Time complexity O(n)
         return solve(root)!=-1;
     }
 };
