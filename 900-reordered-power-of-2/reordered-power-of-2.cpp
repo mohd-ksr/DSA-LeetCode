0f28d0ad832e620cd1cf05598d931490a1a24773
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-10 16:33:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (10.57%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-       string s = to_string(n);
-       sort(s.begin(), s.end());
-       do{
-        if(s[0]=='0')continue;
-        int k = stoi(s);
-        if(((k)&(k-1))==0)return true;
-       }while(next_permutation(s.begin(), s.end()));
+    //    string s = to_string(n);
+    //    sort(s.begin(), s.end());
+    //    do{
+    //     if(s[0]=='0')continue;
+    //     int k = stoi(s);
+    //     if(((k)&(k-1))==0)return true;
+    //    }while(next_permutation(s.begin(), s.end()));
 
-       return false;
+    //    return false;
+
+        unordered_set<string>st;
+        int curr = 1;
+        while(curr<=1e9+7){
+            string s = to_string(curr);
+            sort(s.begin(), s.end());
+            st.insert(s);
+            curr<<=1;
+        }
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        return st.count(s);
     }
 };
