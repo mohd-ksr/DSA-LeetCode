121c7c552e09b615673dfc2107287514751f8c4f
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-12 16:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (46.06%) - LeetSync

---
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int generateKey(int num1, int num2, int num3) {
+        string key = "";
+        while(num1 && num2 && num3){
+            key = to_string(min({num1%10, num2%10, num3%10})) + key;
+            num1/=10;
+            num2/=10;
+            num3/=10;
+        }
+
+        return stoi(key);
+    }
+};
