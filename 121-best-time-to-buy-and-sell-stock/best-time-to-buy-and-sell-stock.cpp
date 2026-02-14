99d2aaa6dec9834943ae96d63bd909506599100b
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-14 09:50:09 +0530

    Time: 188 ms (6.37%) | Memory: 167.5 MB (5.03%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,18 @@
 class Solution {
+private:
+    int solve(int day, vector<int>&prices, int hold, vector<vector<int>>&dp){
+        if(day>=prices.size())return 0;
+        if(dp[day][hold]!=-1)return dp[day][hold];
+        if(hold){
+            return dp[day][hold]=max(prices[day], solve(day+1, prices, hold, dp));
+        }
+        else{
+            return dp[day][hold]=max(-prices[day]+solve(day+1, prices, !hold, dp), solve(day+1, prices, hold, dp));
+        }
+    }
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int ans = 0;
-        int mini = prices[0];
-        for(int i=1; i<n; i++){
-            mini = min(mini, prices[i]);
-            ans = max(ans, prices[i]-mini);
-        }
-        return ans;
+        vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
+        return solve(0, prices, 0, dp);
     }
 };
-// Time complexity O(n)
