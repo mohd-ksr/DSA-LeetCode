542d2bdc6e1ad735cf299d3a9637d58adc5cbe65
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-16 18:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (22.10%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,19 +11,18 @@
  */
 class Solution {
 private:
-    int solve(TreeNode* root, int &maxi){
+    int solve(TreeNode* root, int &ans){
         if(!root)return 0;
-        int l=max(0,solve(root->left,maxi));
-        int r=max(0,solve(root->right,maxi));
-        maxi=max(maxi,root->val+l+r);
-        return root->val + max(l, r);
+        int l = max(0,solve(root->left, ans));
+        int r = max(0,solve(root->right, ans));
+
+        ans = max(ans, root->val+l+r);
+        return root->val+max(l,r);
     }
 public:
     int maxPathSum(TreeNode* root) {
-        int maxi=INT_MIN;
-        solve(root,maxi);
-        return maxi;
+        int ans = INT_MIN;
+        solve(root, ans);
+        return ans;
     }
 };
-// Time complexity O(n)
-// Space complexity O(n)
