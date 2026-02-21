30f7e7ce0c223495869de87469943a8dd39c753c
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-21 09:30:27 +0530

    Time: 56 ms (17.28%) | Memory: 54.1 MB (7.70%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,41 +1,20 @@
 class Solution {
 private:
-    const int inf = 1e9;
-    int solve(int ind, int sum, vector<int>& coins, vector<vector<int>>&dp){
+    int solve(int ind, vector<int>& coins, int sum, vector<vector<int>>&dp){
+        if(ind>=coins.size())return 1e9;
         if(sum==0)return 0;
-        if(ind<=0)return inf;
-        if(dp[ind-1][sum]!=-1)return dp[ind-1][sum];
-        if(coins[ind-1]<=sum){
-            return dp[ind-1][sum]=min(1+solve(ind, sum-coins[ind-1], coins, dp), solve(ind-1, sum, coins, dp));
+        if(dp[ind][sum]!=-1)return dp[ind][sum];
+        if(coins[ind]<=sum){
+            return dp[ind][sum]=min(1+solve(ind, coins, sum-coins[ind], dp), solve(ind+1, coins, sum, dp));
         }
-        else return dp[ind-1][sum]=solve(ind-1, sum, coins, dp);
-
+        else return dp[ind][sum]=solve(ind+1, coins, sum,dp);
     }
 public:
-    int coinChange(vector<int>& coins, int sum) {
-        // int n = coins.size();
-        // vector<vector<int>>dp(n+1, vector<int>(sum+1, -1));
-        // int res = solve(n, sum, coins, dp);
-        // return (res==inf? -1:res);
-
+    int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1, vector<int>(sum+1, inf));
-        for(int i=0; i<=n; i++){
-            dp[i][0]=0;
-        }
+        vector<vector<int>>dp(n+1, vector<int>(amount+1, -1));
+        int res  = solve(0, coins, amount, dp);
+        return (res==1e9)?-1:res;
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=sum; j++){
-                if(coins[i-1]<=j){
-                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-                }
-                else dp[i][j] = dp[i-1][j];
-            }
-        }
-        return (dp[n][sum]==inf? -1:dp[n][sum]);
-        
     }
 };
-
-
-
