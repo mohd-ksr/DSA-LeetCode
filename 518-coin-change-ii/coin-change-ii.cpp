4c21c06bdc8b401fd0ae8062f9ded5d9da122a8b
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-21 12:24:03 +0530

    Time: 51 ms (15.51%) | Memory: 37.1 MB (24.05%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 private:
-    int solve(int ind, int sum, vector<int>& coins,  vector<vector<long long>>&dp){
+    int solve(int ind, int sum, vector<int>& coins, vector<vector<int>>&dp){
+        if(ind>=coins.size())return 0;
         if(sum==0)return 1;
-        if(ind<=0)return 0;
-
-        if(dp[ind-1][sum]!=-1)return dp[ind-1][sum];
-        if(coins[ind-1]<=sum){
-            return dp[ind-1][sum]=solve(ind, sum-coins[ind-1], coins, dp) + solve(ind-1, sum, coins, dp);
+        if(dp[ind][sum]!=-1) return dp[ind][sum];
+        if(coins[ind]<=sum){
+            return dp[ind][sum]=solve(ind, sum-coins[ind], coins, dp) + solve(ind+1, sum, coins, dp);
         }
-        else return dp[ind-1][sum]=solve(ind-1, sum, coins, dp);
+        else return dp[ind][sum]=solve(ind+1, sum, coins, dp);
     }
 public:
-    int change(int sum, vector<int>& coins) {
-
+    int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<long long>>dp(n+1, vector<long long>(sum+1, -1));
-        return solve(n, sum, coins, dp);
+        vector<vector<int>>dp(n+1, vector<int>(amount+1, -1));
+        return solve(0, amount, coins, dp);
     }
 };
