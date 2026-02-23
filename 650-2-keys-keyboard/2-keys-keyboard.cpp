c085a3d2520bff72d3f673e82ca5d74f7da8c843
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-23 12:18:54 +0530

    Time: 63 ms (15.15%) | Memory: 158.9 MB (15.08%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,18 +1,18 @@
 class Solution {
-private: 
-    int solve(int n, int curLen, int pasteLen, vector<vector<int>>&dp){
-        if(curLen==n)return 0;
-        if(curLen>n)return 1000;
-        if(dp[curLen][pasteLen]!=0)return dp[curLen][pasteLen];
-        int opt1 = 2+solve(n, curLen*2, curLen,dp);
-        int opt2 = 1 + solve(n, curLen+pasteLen, pasteLen,dp);
+private:    
+    int solve(int n, int cl, int pl, vector<vector<int>>&dp){
+        if(cl==n)return 0;
+        if(cl>n)return 1000000;
+        if(dp[cl][pl]!=-1)return dp[cl][pl];
+        int opt1 = 2 + solve(n, cl*2, cl, dp);
+        int opt2 = 1 + solve(n, cl+pl, pl, dp);
 
-        return dp[curLen][pasteLen]=min(opt1, opt2);
+        return dp[cl][pl]=min(opt1, opt2);
     }
 public:
     int minSteps(int n) {
         if(n==1)return 0;
-        vector<vector<int>>dp(n+1, vector<int>(n/2+1, 0));
-        return 1+solve(n,1,1,dp);
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        return 1+solve(n, 1, 1, dp);
     }
 };
