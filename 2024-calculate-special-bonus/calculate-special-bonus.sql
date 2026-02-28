54df4478b484e27c2d9779fc0be32784be63dd47
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-28 13:08:19 +0530

    Time: 626 ms (28.87%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,4 +1,1 @@
-select employee_id, salary as bonus from Employees where employee_id%2=1 and name not like "M%"
-union
-select employee_id, 0 as bonus from Employees where employee_id%2=0 or name like "M%"
-order by employee_id;
+select employee_id, salary*(employee_id%2=1)*(name not like "M%") as bonus from Employees order by employee_id;
