11bdcbb65c172d9797e7e43bc968a707903e8ed8
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-03-01 09:26:16 +0530

    Time: 3 ms (49.06%) | Memory: 11 MB (1.89%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.c
@@ -0,0 +1,9 @@
+int minPartitions(char* n) {
+    int ans = 0;
+    for(int i = 0; n[i] != '\0'; i++){
+        int temp = n[i] - '0';
+        ans = (ans > temp ? ans : temp);
+        if(ans == 9) return ans;
+    }
+    return ans;
+}
