644a4764d4cefc44d4a0e35351548f24a9094655
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-03-03 19:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.07%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -13,12 +13,15 @@ private:
     }
 public:
     char findKthBit(int n, int k) {
-        string s = "0";
-        for(int i=2; i<=n; i++){
-            cout<<s<<"\n";
-            string temp = s + "1" + rev(inv(s));
-            s=temp;
-        }
-        return s[k-1];
+        if(n==1)return '0';
+        int len = (1<<n)-1;
+
+        int mid = len/2 + 1;
+
+        if(mid==k)return '1';
+
+        else if(k<mid) return findKthBit(n-1, k);
+
+        return findKthBit(n-1, len-k+1) == '0'? '1':'0';
     }
 };
