e4f39461b7814a9348495109dc2051e3e3366cd8
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-03-13 07:06:25 +0530

    Time: 5 ms (37.56%) | Memory: 18.2 MB (52.91%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>>adj(n);
+        vector<vector<pair<int, int>>>adj(n);
         for(auto f:flights){
-            adj[f[0]].push_back({f[1], f[2]}); // u -> {{v,w}...}
+            int u = f[0];
+            int v = f[1];
+            int w = f[2];
+            adj[u].push_back({v, w});
         }
 
-        queue<pair<int, pair<int, int>>>pq; // (stops, {node, dist})
-        vector<int>dist(n,1e9);
-        pq.push({0, {src, 0}});
-        dist[src]=0;
-        while(!pq.empty()){
-            int s = pq.front().first;
-            auto [node, d] = pq.front().second;
-            pq.pop();
+        vector<int>dist(n, 1e9);
+        queue<pair<int, pair<int, int>>>q;
+        q.push({0, {src, 0}});
+
+        while(!q.empty()){
+            int s = q.front().first;
+            auto [node,d] = q.front().second;
+            q.pop();
             if(s>k)continue;
+
             for(auto [v, w]:adj[node]){
                 if(d+w < dist[v] && s<=k){
-                    dist[v]=d+w;
-                    pq.push({s+1, {v, dist[v]}});
+                    dist[v] = d+w,
+                    q.push({s+1, {v, d+w}});
                 }
             }
         }
-        return (dist[dst]==1e9?-1:dist[dst]);
+        return dist[dst]==1e9? -1:dist[dst];
     }
 };
