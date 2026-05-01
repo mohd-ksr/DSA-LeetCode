222754d75d68c95ba90a5315307805b79a971711
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-05-01 19:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (31.32%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next)return head;
-        
+        if(!head || !head->next)return head; // koi node nhi ya ek node h to kitta bhi rotate kr lo same rahega
+
         ListNode* temp = head;
-        stack<ListNode*>st;
+        stack<ListNode*>st; // starting se last tak satck me push krenge taki last wale stack me top pe rahe
         while(temp){
             st.push(temp);
             temp=temp->next;
         }
-        int size = st.size();
-        k=k%size;
+        int size = st.size(); // satck ka size matlb number of nodes
+        k=k%size; // k ko size ke range me la lo
         
-        while(k--){
-            ListNode* last = st.top();
+        while(k--){ // jab tk k 0 nhi ho jata
+            ListNode* last = st.top(); // last element nikalo stack se
             st.pop();
 
-            last->next=head;
-            head=last;
+            last->next=head; // satrting me connect kr do
+            head=last; // head ko piche le jao matlb starting me 
         }
-        st.top()->next = nullptr;
+        st.top()->next = nullptr; // hamko pta hai stack top pe hamesha last element rahega to last element ka next null kr do
 
-        return head;
+        return head; // or updated head return kr do
     }
 };
