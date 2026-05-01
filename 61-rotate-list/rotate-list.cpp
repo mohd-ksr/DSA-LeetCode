6556c1eb6d3816a395f4c47fb5e3e4f9a80a2c3e
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-05-01 19:11:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (31.32%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,36 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        deque<ListNode*>dq;
+        if(!head || !head->next)return head;
+        
         ListNode* temp = head;
-        while(head){
-            dq.push_back(head); 
-            head=head->next;
+        stack<ListNode*>st;
+        while(temp){
+            st.push(temp);
+            temp=temp->next;
         }
-        if(dq.size()==0 || k==0)return temp;
-        k=k%dq.size();
+        int size = st.size();
+        k=k%size;
+        
         while(k--){
-            ListNode * node = dq.back();
-            dq.pop_back();
-            dq.push_front(node);
+            ListNode* last = st.top();
+            st.pop();
+
+            last->next=head;
+            head=last;
         }
-        head=nullptr;
-        while(!dq.empty()){
-            ListNode * node = dq.back();
-            node->next=head;
-            head=node;
-            dq.pop_back();
-        }
+        st.top()->next = nullptr;
+
         return head;
     }
 };
