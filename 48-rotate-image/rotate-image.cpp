9927a98d7303747c7eca218188bef2cbf4071b81
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-05-04 10:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (8.30%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,23 +1,18 @@
 class Solution {
 private:
-    void reverse(vector<int>&arr){
-        int n=arr.size();
-        for(int i=0; i<n/2; i++){
-            swap(arr[i],arr[n-i-1]);
+    vector<vector<int>>r(vector<vector<int>>&mat){
+        int r = mat.size();
+        int c = mat[0].size();
+        vector<vector<int>>ans(r, vector<int>(c));
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                ans[j][r-i-1] = mat[i][j];
+            }
         }
+        return ans;
     }
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-        for(int i=0; i<n; i++){
-            reverse(matrix[i]);
-        }
+        matrix = r(matrix);
     }
 };
-// Time Complexity O(n²)
-// Space Complexity O(1)
