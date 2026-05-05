bc196d4b7644004d687d8e5e9044beaea8f211f2
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-05-05 22:51:12 +0530

    Time: 10 ms (43.69%) | Memory: 7.8 MB (38.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,12 @@
 class Solution {
-public:
-    int fib(int n) {
+private:
+    int solve(int n){
         if(n<=1)return n;
 
         return fib(n-1) + fib(n-2);
+    }
+public:
+    int fib(int n) {
+        return solve(n);
     }
 };
