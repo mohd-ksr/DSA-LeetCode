7ded3a89e77198473a1b64edae7a8d7b501c351b
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-05-05 23:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (20.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,18 @@
 class Solution {
 private:
-    int solve(vector<int>& nums, int ind, vector<int>&dp){
+    int solve(int ind, vector<int>& nums,bool PisTaken, vector<vector<int>>&dp){
         if(ind>=nums.size())return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        return dp[ind]=max(nums[ind]+solve(nums,ind+2, dp), solve(nums,ind+1, dp));
+        if(dp[ind][PisTaken]!=-1)return dp[ind][PisTaken];
+        if(PisTaken){
+            return dp[ind][PisTaken]=solve(ind+1, nums, false, dp);
+        }
+        else{
+            return dp[ind][PisTaken]=max(nums[ind]+solve(ind+1, nums, true, dp), solve(ind+1, nums, false, dp));
+        }
     }
 public:
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1, -1);
-        return solve(nums, 0, dp);
+        vector<vector<int>>dp(nums.size()+1, vector<int>(2, -1));
+        return solve(0, nums, false, dp);
     }
 };
