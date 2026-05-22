53448b375b10474c4f09e2cd933534244327d097
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-05-22 16:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,19 +1,25 @@
 class Solution {
 private:
-    int findP(vector<int>&nums){
-        int s = 0, e = nums.size()-1;
+    int getP(vector<int>& nums){
+        int s=0, e = nums.size()-1;
         while(s<e){
             int mid = s+(e-s)/2;
-            if(nums[mid]>=nums[0])s=mid+1;
-            else e=mid;
+            if(nums[mid]>=nums[0]){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
         }
         return s;
     }
-    int bs(vector<int>&nums, int s, int e, int target){
+    int bs(vector<int>& nums, int target, int s, int e){
         while(s<=e){
             int mid = s+(e-s)/2;
             if(nums[mid]==target)return mid;
-            else if(nums[mid]>target)e=mid-1;
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
             else s=mid+1;
         }
         return -1;
@@ -21,11 +27,12 @@ private:
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int p = findP(nums);
-        cout<<p<<" ";
-        if(target >= nums[p] && target<= nums[n-1]){
-            return bs(nums, p, n-1, target);
+        int p = getP(nums);
+        if(target>=nums[p] && target<=nums[n-1]){
+            return bs(nums, target, p, n-1);
         }
-        else return bs(nums, 0, p-1, target);
+        else{
+            return bs(nums, target, 0, p-1);
+        }
     }
 };
