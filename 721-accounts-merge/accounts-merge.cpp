d2d4177e9e9ed09790616c16cd8f1dfeeb8bd53d
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-06-24 12:23:52 +0530

    Time: 27 ms (79.15%) | Memory: 44.2 MB (24.76%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,26 +1,29 @@
 class DSU{
 private:
-    vector<int>parent, rank;
+    vector<int>rank,parent;
 public:
     DSU(int n){
+        rank.resize(n+1, 0);
         parent.resize(n+1);
-        rank.resize(n+1,0);
-        for(int i=0; i<=n;i++)parent[i]=i;
+        for(int i=0; i<=n; i++){
+            parent[i]=i;
+        }
     }
-
-    int findUpar(int node){
-        if(parent[node]==node)return node;
-        return parent[node] = findUpar(parent[node]);
+    
+    int findUlpParent(int node){
+        if(node==parent[node])return node;
+        return parent[node]=findUlpParent(parent[node]);
     }
-
+    
     void unionByRank(int u, int v){
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
-
+        int ulp_u = findUlpParent(u);
+        int ulp_v = findUlpParent(v);
+        if(ulp_u==ulp_v)return;
+        
         if(rank[ulp_u] < rank[ulp_v]){
             parent[ulp_u] = ulp_v;
         }
-        else if(rank[ulp_u] > rank[ulp_v]){
+        else if(rank[ulp_v] < rank[ulp_u]){
             parent[ulp_v] = ulp_u;
         }
         else{
@@ -29,44 +32,46 @@ public:
         }
     }
 };
+
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
-        DSU ds(n);
         unordered_map<string, int>mapp;
-
+        DSU ds(n);
         for(int i=0; i<n; i++){
             for(int j=1; j<accounts[i].size(); j++){
-                string email = accounts[i][j];
-                if(mapp.find(email)==mapp.end()){
-                    mapp[email] = i;
+                string mail = accounts[i][j];
+                if(mapp.find(mail)==mapp.end()){
+                    mapp[mail]=i;
                 }
                 else{
-                    ds.unionByRank(i, mapp[email]);
+                    ds.unionByRank(i, mapp[mail]);
                 }
             }
         }
         unordered_map<int, vector<string>>mapp2;
         for(auto it:mapp){
-            string email = it.first;
-            int node = ds.findUpar(it.second);
-            mapp2[node].push_back(email);
+            string mail = it.first;
+            int node = ds.findUlpParent(it.second);
+            mapp2[node].push_back(mail);
         }
 
-        vector<vector<string>>ans;
 
+        vector<vector<string>>ans;
         for(auto it:mapp2){
-            vector<string>temp;
-            int node = it.first;
-            temp.push_back(accounts[node][0]);
-            vector<string>t = it.second;
-            sort(t.begin(), t.end());
-            for(auto e:t){
-                temp.push_back(e);
-            }
-            ans.push_back(temp);
+            int ind = it.first;
+            vector<string>mails = it.second;
+            sort(mails.rbegin(), mails.rend());
+            mails.push_back(accounts[ind][0]);
+            reverse(mails.begin(), mails.end());
+            ans.push_back(mails);
         }
         return ans;
     }
 };
+
+
+
+
+
