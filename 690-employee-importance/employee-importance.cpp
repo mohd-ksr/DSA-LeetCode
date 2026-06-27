a0990977b9232cbf62bd17efbc3ce2a52b1029de
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-06-27 21:42:14 +0530

    Time: 32 ms (12.19%) | Memory: 22.2 MB (23.32%) - LeetSync

--- 690-employee-importance/employee-importance.cpp
+++ 690-employee-importance/employee-importance.cpp
@@ -12,22 +12,31 @@ class Solution {
 private:
     unordered_map<int, int>imp;
     unordered_map<int, vector<int>>adj;
+    int dfs(int node){
+        if(adj[node].empty())return imp[node];
+        int childsImp = 0;
+        for(auto it:adj[node]){
+            childsImp+=dfs(it);
+        } 
+        return imp[node]+childsImp;
+    }
 public:
     int getImportance(vector<Employee*> employees, int id) {
         for(auto emp:employees){
             this->imp[emp->id]=emp->importance;
             this->adj[emp->id]=emp->subordinates;
         }
-        int ans=0;
-        queue<int>q;
-        q.push(id);
-        while(!q.empty()){
-            int node = q.front();q.pop();
-            ans+=imp[node];
-            for(auto it:adj[node]){
-                q.push(it);
-            }
-        }
-        return ans;
+        return dfs(id);
+        // int ans=0;
+        // queue<int>q;
+        // q.push(id);
+        // while(!q.empty()){
+        //     int node = q.front();q.pop();
+        //     ans+=imp[node];
+        //     for(auto it:adj[node]){
+        //         q.push(it);
+        //     }
+        // }
+        // return ans;
     }
 };
