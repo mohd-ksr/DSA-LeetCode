5f180b6bb02ce21b6bceaaac8e820d0e2b0ff701
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-07-12 23:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (62.15%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n=digits.size();
-        for(int i=n-1; i>=0; --i){
-            if(digits[i]<9){
-                digits[i]++;
-                return digits;
-            }
-            digits[i]=0;
+        reverse(digits.begin(), digits.end());
+        int carry = 1;
+        for(int i=0; i<digits.size(); i++){
+            int num = digits[i];
+            digits[i] = (num + carry)%10;
+            carry = (num+carry)/10;
+            if(carry==0)break;
         }
-        digits.insert(digits.begin(),1);
+        if(carry==1)digits.push_back(1);
+        reverse(digits.begin(), digits.end());
         return digits;
     }
 };
-// Time Complexity: O(n)
-// Space Complexity: O(1)
