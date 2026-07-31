491795f388743daf62998fa0f4e9be66a97b0afc
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-07-31 23:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (45.71%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,24 @@
 class Solution {
 private:
-    vector<string>result;
-    void solve(int i, string &digits, string &temp, unordered_map<char,string>&mapp){
-        if(i>=digits.length()){
-            result.push_back(temp);
+    vector<string>mapp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string>ans;
+    void solve(int ind, string &digits, string temp){
+        if(ind>=digits.size()){
+            ans.push_back(temp);
             return;
         }
 
-        char ch=digits[i];
-        string str=mapp[ch];
-
-        for(int j=0; j<str.length(); j++){
-            temp.push_back(str[j]);
-            solve(i+1,digits,temp,mapp);
+        int d = digits[ind]-'0';
+        for(auto ch:mapp[d]){
+            temp.push_back(ch);
+            solve(ind+1, digits, temp);
             temp.pop_back();
         }
+
     }
 public:
     vector<string> letterCombinations(string digits) {
-        if(digits.length()<=0)return {};
-        unordered_map<char,string>mapp;
-        mapp['2']="abc";
-        mapp['3']="def";
-        mapp['4']="ghi";
-        mapp['5']="jkl";
-        mapp['6']="mno";
-        mapp['7']="pqrs";
-        mapp['8']="tuv";
-        mapp['9']="wxyz";
-        string temp="";
-        solve(0,digits,temp,mapp);
-        return result;
+        solve(0, digits, "");
+        return ans;
     }
 };
-// Time Complexity O(4^n)
-// Space Complexity O(n)
