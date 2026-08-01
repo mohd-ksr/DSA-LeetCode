166c8246c39a7428bb584406bd005f010e041678
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-08-01 15:20:25 +0530

    Time: 135 ms (8.59%) | Memory: 71.9 MB (6.22%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,21 +1,20 @@
 class Solution {
 private:
+    unordered_map<string, bool>dp;
     bool solve(int s, int e, int p1, int p2, bool isP1,vector<int>& nums){
         if(s>e){
             return p1>=p2;
         }
-        bool start, end;
+        string key = to_string(s)+"#"+to_string(e)+"#"+to_string(p1)+"#"+to_string(p2);
+        if(dp.count(key))return dp[key];
+        bool ans;
         if(isP1){
-            start = solve(s+1, e, p1+nums[s], p2, false, nums);
-            end = solve(s, e-1, p1+nums[e], p2, false, nums);
-            return start || end;
+            ans = solve(s+1, e, p1+nums[s], p2, false, nums)||solve(s, e-1, p1+nums[e], p2, false, nums);
         }
         else{
-            start = solve(s+1, e, p1, p2+nums[s], true, nums);
-            end = solve(s, e-1, p1, p2+nums[e], true, nums);
-            return start && end;
+            ans = solve(s+1, e, p1, p2+nums[s], true, nums)&&solve(s, e-1, p1, p2+nums[e], true, nums);
         }
-        
+        return dp[key]=ans;   
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
