352a26eeee1eaf4a038dc51081d262daac5d5338
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-08-02 11:17:17 +0530

    Time: 694 ms (5.00%) | Memory: 117.8 MB (5.00%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
+private:
+    unordered_map<string, int>dp;
+    int solve(int s, int e,vector<int>&piles){
+        if(s==e){
+            return piles[s];
+        }
+        string key = to_string(s)+"#"+to_string(e);
+        if(dp.count(key))return dp[key];
+
+        int l = piles[s] - solve(s+1, e, piles);
+        int r = piles[e] - solve(s, e-1, piles);
+        return dp[key]=max(l,r);
+    }
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        return solve(0, piles.size()-1,piles)>0;
     }
 };
