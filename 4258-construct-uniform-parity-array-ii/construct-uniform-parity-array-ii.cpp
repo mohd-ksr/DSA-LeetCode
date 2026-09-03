da43b7f14ffb48dc009de721c8dcd7653d5e4332
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-09-03 22:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 165.8 MB (62.83%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        bool odd=true, even=true;
+
+        int smallestOdd = INT_MAX, smallestEven = INT_MAX;
+        for(auto num:nums1){
+            if(num%2==0 && num<smallestEven){
+                smallestEven=num;
+            }
+            if(num%2==1 && num<smallestOdd){
+                smallestOdd=num;
+            }
+        }
+        //construct with odd
+        for(auto num:nums1){
+            if(num%2==1 || num-smallestOdd>=1)continue;
+            odd=false;
+            break;
+        }
+        //construct with even 
+        for(auto num:nums1){
+            if(num%2==0 || num-smallestOdd>=1)continue;
+            even=false;
+            break;
+        }
+        return odd||even;
+    }
+};
