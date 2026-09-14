4699e7418206a04ffce9c3b983ba7aa1c3f80e4f
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-09-14 16:24:24 +0530

    Time: 7 ms (73.67%) | Memory: 35 MB (43.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,39 +1,32 @@
 class Solution {
 private:
     int r, c;
-    void dfs(vector<vector<int>>& grid, int i, int j, vector<vector<bool>>&seen){
-        if(i<0 || i>=r || j<0 || j>=c || seen[i][j] || grid[i][j]==0)return;
-        seen[i][j]=true;
-        dfs(grid, i-1, j, seen);
-        dfs(grid, i, j-1, seen);
-        dfs(grid, i+1, j, seen);
-        dfs(grid, i, j+1, seen);
+    void dfs(int i, int j, vector<vector<int>>& grid){
+        if(i<0 || i>=r || j<0 || j>=c || grid[i][j]!=1)return;
+        grid[i][j]=-1;
+        dfs(i-1, j, grid);
+        dfs(i, j-1, grid);
+        dfs(i+1, j, grid);
+        dfs(i, j+1, grid);
     }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        this->r=grid.size();
-        this->c=grid[0].size();
-
-        vector<vector<bool>>seen(r, vector<bool>(c, false));
-
-        for(int i=0; i<r; i++){
-            if(grid[i][0]==1 && !seen[i][0])dfs(grid, i, 0, seen);
-            if(grid[i][c-1]==1 && !seen[i][c-1])dfs(grid, i, c-1, seen);
-        }
+        this->r = grid.size();
+        this->c = grid[0].size();
         for(int j=0; j<c; j++){
-            if(grid[0][j]==1 && !seen[0][j])dfs(grid, 0, j, seen);
-            if(grid[r-1][j]==1 && !seen[r-1][j])dfs(grid, r-1, j, seen);
+            if(grid[0][j]==1)dfs(0, j, grid);
+            if(grid[r-1][j]==1)dfs(r-1, j, grid);
         }
-
-        int totalOnes = 0;
-        int boundryOnes = 0;
+        for(int i=0; i<r; i++){
+            if(grid[i][0]==1)dfs(i, 0, grid);
+            if(grid[i][c-1]==1)dfs(i, c-1, grid);
+        }
+        int ans=0;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                totalOnes+=(grid[i][j]==1);
-                boundryOnes+=(seen[i][j]);
+                ans+=(grid[i][j]==1);
             }
         }
-
-        return totalOnes-boundryOnes;
+        return ans;
     }
 };
